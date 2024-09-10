ce7114b785b216ee661d3ee7bf1b5bbc3b3e72bf
phas02 <suter.philipp@gmail.com>
2024-09-10 18:56:12 +0200

    added some color

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,10 +2,12 @@
 	<div class="place-self-center">
 		<img src="/logo.webp" class="h-96 sm:h-96" alt="tocq.io logo" />
 	</div>
-	<div class="place-self-center"><h1 class="text-3xl font-bold">Work in progress</h1></div>
+	<div class="place-self-center">
+		<h1 class="text-3xl font-bold text-zinc-900">Work in progress...</h1>
+	</div>
 	<div class="place-self-center">
 		<a href="https://myspace.tocq.io" target="_blank"
-			><h2 class="text-2xl font-bold underline">Playground running in the browser</h2></a
+			><h2 class="text-2xl font-bold underline text-fuchsia-900">Try the playground</h2></a
 		>
 	</div>
 </div>
