3dcbc62458e746d9f16b4da4d9bd18e8ed6754da
phas02 <suter.philipp@gmail.com>
2024-09-10 18:35:29 +0200

    homepage placeholder

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,13 +1,7 @@
 // See https://kit.svelte.dev/docs/types#app
 // for information about these interfaces
 declare global {
-	namespace App {
-        interface Platform {
-            env: Env
-            cf: CfProperties
-            ctx: ExecutionContext
-        }
-    }
+	namespace App {}
 }
 
 export {};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,14 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="grid grid-cols-1 place-content-evenly h-screen w-screen">
+	<div class="place-self-center">
+		<img src="/logo.webp" class="h-96 sm:h-96" alt="tocq.io logo" />
+	</div>
+	<div class="place-self-center"><h1 class="text-3xl font-bold">Work in progress</h1></div>
+	<div class="place-self-center">
+		<a href="https://myspace.tocq.io" target="_blank"
+			><h2 class="text-2xl font-bold underline">Playground running in the browser</h2></a
+		>
+	</div>
+</div>
+
+<style>
+</style>
